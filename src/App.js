1307749c0a79018055eba801e396a34ac56fa8b8
Alexandru Munteanu <amunteanu@umass.edu>
2020-03-11 15:32:06 -0400

    Add currentTime example to react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +19,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>The current time is {currentTime}.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
