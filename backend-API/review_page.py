d51c40964031d58a5b093125ebb18ed399454c88
99alecb <61883272+99alecb@users.noreply.github.com>
2020-04-18 16:41:57 -0400

    Backend Setup
    Hopefully I didn't miss anything while doing this

--- backend-API/review_page.py
+++ backend-API/review_page.py
@@ -1,2 +1,7 @@
+from pymongo import MongoClient
+from flask import jsonify
+
+client = MongoClient('mongodb://localhost:27017/')
+
 def get_reviews(username):
     return None
--- windows_setup.ps1
+++ windows_setup.ps1
@@ -2,6 +2,6 @@ yarn
 cd backend-API
 python -m venv venv
 venv\Scripts\activate
-pip install flask python-dotenv flask_jwt_extended
+pip install flask python-dotenv flask_jwt_extended pymongo
 deactivate
 cd ..
