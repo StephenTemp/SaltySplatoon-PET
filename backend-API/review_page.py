f1f87cd83c0e8d4902226e79bd51ed06d857b97d
jye6013 <jye6013@bths.edu>
2020-04-19 21:35:58 -0400

    fix get user reviews

--- backend-API/review_page.py
+++ backend-API/review_page.py
@@ -7,30 +7,31 @@ client = MongoClient('mongodb://localhost:27017/')
 def get_user_reviews(email):
     PET_db = client["PET"]
 
-    # Create a review list
+    # initialize review list
     reviews_list = [] 
 
-    # Get review list
+    # get requests and review_content data
+    requests = PET_db["requests"]
     review_content = PET_db["review_content"]
 
-    # Get user information
+    # get current user data
     employee_data = PET_db["employee_data"]
     current_employee = employee_data.find_one({"email": email})
-    
     cur_employee_id = current_employee["employeeId"]
     cur_employee_company_id = current_employee["companyId"]
 
-    for review in review_content.find({"reviewer_id": cur_employee_id, "companyId": cur_employee_company_id}):
-        # If the one being reviewed is same as the user
-        if review["email"] == current_employee["email"]: 
-            # Get reviewer
-            cur_reviewer = employee_data.find_one({"employeeId": review["reviewer_id"]}) 
-             # Get reviewer name
+    # search for reviews
+    for review in requests.find({"requester.id": cur_employee_id, "companyId": cur_employee_company_id}):
+        # only if review is complete then add
+        if(review["complete"]):
+            # get reviewer data and review content and append
+            cur_reviewer = employee_data.find_one({"employeeId": review["reviewer_id"]})
             cur_reviewer_name = cur_reviewer["firstName"] + " " + cur_reviewer["lastName"]
-            # Append reviewer, review, data
-            reviews_list.append({"reviewer": cur_reviewer_name, "review": review["content"], "date": review["date"]}) 
+            cur_review = review_content.find_one({"_id": review["review_content_id"]})
+            reviews_list.append({"reviewer": cur_reviewer_name, "review": cur_review["content"], "date": cur_review["date"]})
 
-    return jsonify(reviews_list=reviews_list), 200
+    return jsonify(reviews_list=reviews_list), 200 # add more if needed
 
 # def get_employee_reviews(email):
+
 
