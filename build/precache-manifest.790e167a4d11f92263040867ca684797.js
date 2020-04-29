8434339af026cd7888e7b7a090d6e9d5d6cff937
Alexandru Munteanu <amunteanu@umass.edu>
2020-04-29 17:30:59 -0400

    Added a production build folder

---
+++ build/precache-manifest.790e167a4d11f92263040867ca684797.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a6caaf49cd03bd8917b718e8a68c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f664f1c38e3d21777470",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "386478c13e0f8329f7bf",
+    "url": "/static/css/main.9a89e1aa.chunk.css"
+  },
+  {
+    "revision": "f664f1c38e3d21777470",
+    "url": "/static/js/2.7965ef23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7965ef23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386478c13e0f8329f7bf",
+    "url": "/static/js/main.f46b06a8.chunk.js"
+  },
+  {
+    "revision": "960ad12d49e48946318c",
+    "url": "/static/js/runtime-main.75bac790.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
